b9b7374ccc84d22bb657893de702ce50db686406
Victor Villazón <victorzn23@gmail.com>
2023-03-13 15:37:25 -0500

    refactor: remove unnecessary tests

--- components/Pokemons.test.tsx
+++ components/Pokemons.test.tsx
@@ -24,9 +24,9 @@ export const Pokemons: React.FC<Props> = ({ pokemons }) => {
 describe('Pokemons', () => {
   beforeEach(cleanup)
 
-  it('should render', () => {
-    render(<Pokemons pokemons={[]} />)
-  })
+  // it('should render', () => {
+  //   render(<Pokemons pokemons={[]} />)
+  // })
 
   it('should render a pokemon name', () => {
     const mockPokemons = [{
@@ -39,16 +39,16 @@ describe('Pokemons', () => {
     screen.getByText('pokemon one')
   })
 
-  it('should render another pokemon', () => {
-    const mockPokemons = [{
-      id: 1,
-      name: 'pokemon two'
-    }]
+  // it('should render another pokemon', () => {
+  //   const mockPokemons = [{
+  //     id: 1,
+  //     name: 'pokemon two'
+  //   }]
 
-    render(<Pokemons pokemons={mockPokemons} />)
+  //   render(<Pokemons pokemons={mockPokemons} />)
 
-    screen.getByText('pokemon two')
-  })
+  //   screen.getByText('pokemon two')
+  // })
 
   it('should render multiple pokemon names', () => {
     const pokemons = [
