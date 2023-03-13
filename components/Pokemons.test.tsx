fc6133784ef765b95f7c80efc85592ee364dfbcb
Victor Villazón <victorzn23@gmail.com>
2023-03-13 15:15:55 -0500

    test: Pokemons - it should render, passed

--- components/Pokemons.test.tsx
+++ components/Pokemons.test.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import { describe, it, beforeEach, expect } from 'vitest'
 
+export const Pokemons: React.FC = () => {
+  return null
+}
+
 describe('Pokemons', () => {
   it('should render', () => {
     render(<Pokemons />)
