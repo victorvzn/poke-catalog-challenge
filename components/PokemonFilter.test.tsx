e9fc4c32b252bb80ce5d64fa33e559070c9fd536
Victor Villazón <victorzn23@gmail.com>
2023-03-13 18:18:21 -0500

    feat: PokemonSearch - it should allow writing text in a text box, passed

--- components/PokemonFilter.test.tsx
+++ components/PokemonFilter.test.tsx
@@ -3,11 +3,45 @@ import { render, screen, cleanup } from '@testing-library/react'
 import { describe, it, beforeEach, expect, vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
+export interface PokemonFilter {
+  name: string
+}
+
+const INITIAL_STATE = {
+  name: ''
+}
+
+interface FormState {
+  inputValue: PokemonFilter
+}
+
 export const PokemonSearch: React.FC = () => {
-  return null
+  const [inputValue, setInputValue] = useState<FormState['inputValue']>(INITIAL_STATE)
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target
+
+    setInputValue(prevState => ({ ...prevState, [name]: value }))
+  }
+
+  return (
+    <form>
+      <label>
+        <input
+          name='name'
+          type='text'
+          placeholder='Name or number'
+          onChange={handleChangeInput}
+          value={inputValue.name}
+        />
+      </label>
+    </form>
+  )
 }
 
 describe('PokemonSearch', () => {
+  beforeEach(cleanup)
+
   it('should render', () => {
     render(<PokemonSearch />)
   })
@@ -20,8 +54,6 @@ describe('PokemonSearch', () => {
     const input = screen.getByPlaceholderText('Name or number')
 
     await user.type(input, 'oops{backspace}{backspace}{backspace}{backspace}geodude')
-
-    screen.debug()
 
     expect(input.getAttribute('value')).toBe('geodude')
   })
