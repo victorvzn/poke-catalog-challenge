a09cc3598ab71f281acbfa06cb77660bdc8bf4e8
Victor Villazón <victorzn23@gmail.com>
2023-03-13 15:22:48 -0500

    test: Pokemons - should render a pokemon name, passed

--- components/Pokemons.test.tsx
+++ components/Pokemons.test.tsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import { describe, it, beforeEach, expect } from 'vitest'
+import { before } from 'node:test'
 
-export const Pokemons: React.FC = () => {
-  return null
+export interface Pokemon {
+  name: string
+}
+
+interface Props {
+  pokemons?: Pokemon[]
+}
+
+export const Pokemons: React.FC<Props> = ({ pokemons }) => {
+  return (
+    <div>pokemon one</div>
+  )
 }
 
 describe('Pokemons', () => {
+  beforeEach(cleanup)
+
   it('should render', () => {
     render(<Pokemons />)
   })
 
   it('should render a pokemon name', () => {
-    const pokemons = [{
-      id: 1,
+    const mockPokemons = [{
       name: 'pokemon one'
     }]
 
-    render(<Pokemons pokemons={pokemons} />)
+    render(<Pokemons pokemons={mockPokemons} />)
 
     screen.getByText('pokemon one')
   })
