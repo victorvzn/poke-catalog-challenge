70a4c7cf36d71ada0bdea63d23e7a2aafae58502
Victor Villazón <victorzn23@gmail.com>
2023-03-13 18:13:12 -0500

    chore: PokemonSearch - it should render, passed

--- components/PokemonFilter.test.tsx
+++ components/PokemonFilter.test.tsx
@@ -3,6 +3,10 @@ import { render, screen, cleanup } from '@testing-library/react'
 import { describe, it, beforeEach, expect, vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
+export const PokemonSearch: React.FC = () => {
+  return null
+}
+
 describe('PokemonSearch', () => {
   it('should render', () => {
     render(<PokemonSearch />)
