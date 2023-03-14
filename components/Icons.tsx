eb7ef72aedadf5a0ef6cd87f108a9becce9f0a4d
Victor Villazón <victorzn23@gmail.com>
2023-03-14 03:00:05 -0500

    chore: update SVG icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -6,72 +6,72 @@ interface Props {
 
 export const PokeBallIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' fill='none'><g clip-path='url(#a)'><path fill='#ED5564' d='M26.9 8.55a13.956 13.956 0 0 0-3-4.45A13.97 13.97 0 0 0 14 0a13.958 13.958 0 0 0-9.9 4.1A13.958 13.958 0 0 0 0 14a13.957 13.957 0 0 0 4.1 9.9A13.945 13.945 0 0 0 14 28a13.959 13.959 0 0 0 9.9-4.1A13.957 13.957 0 0 0 28 14c0-1.89-.37-3.723-1.1-5.45Z' /><path fill='#E6E9ED' d='M.006 14.393A13.958 13.958 0 0 0 4.1 23.9a13.942 13.942 0 0 0 4.45 3c1.726.73 3.56 1.1 5.449 1.1a13.957 13.957 0 0 0 9.9-4.1 13.957 13.957 0 0 0 4.095-9.506H.005Z' /><path fill='#434A54' d='M27.933 15.38a14.234 14.234 0 0 0-.017-2.927c-.695.107-1.694.247-2.953.386-2.39.263-6.23.578-10.963.578-4.733 0-8.574-.315-10.963-.578a69.378 69.378 0 0 1-2.953-.386 14.2 14.2 0 0 0-.017 2.926c2.27.342 7.275.954 13.933.954 6.658 0 11.662-.612 13.933-.954Z' /><path fill='#E6E9ED' d='M16.917 14.583a2.917 2.917 0 1 1-5.834 0 2.917 2.917 0 0 1 5.834 0Z' /><path fill='#434A54' d='M14 9.333a5.256 5.256 0 0 0-5.25 5.25 5.256 5.256 0 0 0 5.25 5.25 5.256 5.256 0 0 0 5.25-5.25A5.256 5.256 0 0 0 14 9.333Zm0 7.584a2.337 2.337 0 0 1-2.333-2.334A2.336 2.336 0 0 1 14 12.25a2.335 2.335 0 0 1 2.333 2.333A2.336 2.336 0 0 1 14 16.917Z' /><path fill='#fff' d='M26.9 8.55a13.956 13.956 0 0 0-3-4.45A13.97 13.97 0 0 0 13.417.012 13.943 13.943 0 0 1 22.733 4.1a13.957 13.957 0 0 1 4.1 9.9 13.958 13.958 0 0 1-4.1 9.9 13.93 13.93 0 0 1-4.45 3 13.886 13.886 0 0 1-4.866 1.088A13.92 13.92 0 0 0 19.45 26.9a13.96 13.96 0 0 0 4.45-3A13.958 13.958 0 0 0 28 14c0-1.89-.37-3.723-1.1-5.45Z' opacity='.2' /><path fill='#000' d='M1.1 19.45a13.957 13.957 0 0 0 3 4.45A13.945 13.945 0 0 0 14 28c.195 0 .39-.004.583-.012A13.943 13.943 0 0 1 5.267 23.9 13.96 13.96 0 0 1 1.167 14a13.951 13.951 0 0 1 4.1-9.9A13.95 13.95 0 0 1 14.583.013 13.911 13.911 0 0 0 8.55 1.101a13.952 13.952 0 0 0-4.45 3A13.951 13.951 0 0 0 0 14c0 1.89.37 3.723 1.1 5.449Z' opacity='.1' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h28v28H0z' /></clipPath></defs></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' fill='none'><g clipPath='url(#a)'><path fill='#ED5564' d='M26.9 8.55a13.956 13.956 0 0 0-3-4.45A13.97 13.97 0 0 0 14 0a13.958 13.958 0 0 0-9.9 4.1A13.958 13.958 0 0 0 0 14a13.957 13.957 0 0 0 4.1 9.9A13.945 13.945 0 0 0 14 28a13.959 13.959 0 0 0 9.9-4.1A13.957 13.957 0 0 0 28 14c0-1.89-.37-3.723-1.1-5.45Z' /><path fill='#E6E9ED' d='M.006 14.393A13.958 13.958 0 0 0 4.1 23.9a13.942 13.942 0 0 0 4.45 3c1.726.73 3.56 1.1 5.449 1.1a13.957 13.957 0 0 0 9.9-4.1 13.957 13.957 0 0 0 4.095-9.506H.005Z' /><path fill='#434A54' d='M27.933 15.38a14.234 14.234 0 0 0-.017-2.927c-.695.107-1.694.247-2.953.386-2.39.263-6.23.578-10.963.578-4.733 0-8.574-.315-10.963-.578a69.378 69.378 0 0 1-2.953-.386 14.2 14.2 0 0 0-.017 2.926c2.27.342 7.275.954 13.933.954 6.658 0 11.662-.612 13.933-.954Z' /><path fill='#E6E9ED' d='M16.917 14.583a2.917 2.917 0 1 1-5.834 0 2.917 2.917 0 0 1 5.834 0Z' /><path fill='#434A54' d='M14 9.333a5.256 5.256 0 0 0-5.25 5.25 5.256 5.256 0 0 0 5.25 5.25 5.256 5.256 0 0 0 5.25-5.25A5.256 5.256 0 0 0 14 9.333Zm0 7.584a2.337 2.337 0 0 1-2.333-2.334A2.336 2.336 0 0 1 14 12.25a2.335 2.335 0 0 1 2.333 2.333A2.336 2.336 0 0 1 14 16.917Z' /><path fill='#fff' d='M26.9 8.55a13.956 13.956 0 0 0-3-4.45A13.97 13.97 0 0 0 13.417.012 13.943 13.943 0 0 1 22.733 4.1a13.957 13.957 0 0 1 4.1 9.9 13.958 13.958 0 0 1-4.1 9.9 13.93 13.93 0 0 1-4.45 3 13.886 13.886 0 0 1-4.866 1.088A13.92 13.92 0 0 0 19.45 26.9a13.96 13.96 0 0 0 4.45-3A13.958 13.958 0 0 0 28 14c0-1.89-.37-3.723-1.1-5.45Z' opacity='.2' /><path fill='#000' d='M1.1 19.45a13.957 13.957 0 0 0 3 4.45A13.945 13.945 0 0 0 14 28c.195 0 .39-.004.583-.012A13.943 13.943 0 0 1 5.267 23.9 13.96 13.96 0 0 1 1.167 14a13.951 13.951 0 0 1 4.1-9.9A13.95 13.95 0 0 1 14.583.013 13.911 13.911 0 0 0 8.55 1.101a13.952 13.952 0 0 0-4.45 3A13.951 13.951 0 0 0 0 14c0 1.89.37 3.723 1.1 5.449Z' opacity='.1' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h28v28H0z' /></clipPath></defs></svg>
   )
 }
 
 export const PokeBallDisabledIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' fill='none'><path fill='#BFBFBF' fill-rule='evenodd' d='M0 14C0 6.268 6.268 0 14 0s14 6.268 14 14-6.268 14-14 14S0 21.732 0 14Zm3.221 1.556c.755 5.277 5.293 9.333 10.779 9.333s10.024-4.056 10.779-9.333H18.4a4.669 4.669 0 0 1-8.802 0H3.22Zm21.558-3.112C24.024 7.168 19.486 3.111 14 3.111S3.976 7.168 3.221 12.444H9.6a4.669 4.669 0 0 1 8.802 0h6.378ZM14 15.556a1.556 1.556 0 1 0 0-3.112 1.556 1.556 0 0 0 0 3.112Z' clip-rule='evenodd' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' fill='none'><path fill='#BFBFBF' fillRule='evenodd' d='M0 14C0 6.268 6.268 0 14 0s14 6.268 14 14-6.268 14-14 14S0 21.732 0 14Zm3.221 1.556c.755 5.277 5.293 9.333 10.779 9.333s10.024-4.056 10.779-9.333H18.4a4.669 4.669 0 0 1-8.802 0H3.22Zm21.558-3.112C24.024 7.168 19.486 3.111 14 3.111S3.976 7.168 3.221 12.444H9.6a4.669 4.669 0 0 1 8.802 0h6.378ZM14 15.556a1.556 1.556 0 1 0 0-3.112 1.556 1.556 0 0 0 0 3.112Z' clipRule='evenodd' /></svg>
   )
 }
 
 export const SettingsIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><g stroke='#fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' clip-path='url(#a)'><path d='M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z' /><path d='M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a1.998 1.998 0 0 1 0 2.83 1.998 1.998 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a1.998 1.998 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 3.417 1.415 2 2 0 0 1-.587 1.415l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1Z' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h24v24H0z' /></clipPath></defs></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><g stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' clipPath='url(#a)'><path d='M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z' /><path d='M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a1.998 1.998 0 0 1 0 2.83 1.998 1.998 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a1.998 1.998 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 3.417 1.415 2 2 0 0 1-.587 1.415l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1Z' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h24v24H0z' /></clipPath></defs></svg>
   )
 }
 
 export const SearchIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><path stroke='#BFBFBF' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M11 19a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM21 21l-4.35-4.35' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><path stroke='#BFBFBF' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M11 19a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM21 21l-4.35-4.35' /></svg>
   )
 }
 
 export const FilterIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#BFBFBF' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.667' d='M18.333 2.5H1.667l6.666 7.883v5.45l3.334 1.667v-7.117L18.333 2.5Z' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#BFBFBF' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.667' d='M18.333 2.5H1.667l6.666 7.883v5.45l3.334 1.667v-7.117L18.333 2.5Z' /></svg>
   )
 }
 
 export const ChevronDownIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#BFBFBF' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.667' d='m5 7.5 5 5 5-5' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#BFBFBF' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.667' d='m5 7.5 5 5 5-5' /></svg>
   )
 }
 
 export const ListIcon: React.FC<Props> = ({ stroke = '#BFBFBF' }) => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><path stroke={stroke} stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M8 6h13M8 12h13M8 18h13M3 6h.01M3 12h.01M3 18h.01' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><path stroke={stroke} strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 6h13M8 12h13M8 18h13M3 6h.01M3 12h.01M3 18h.01' /></svg>
   )
 }
 
 export const ColumnsIcon: React.FC<Props> = ({ stroke = '#BFBFBF' }) => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><path stroke={stroke} stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 3h7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-7m0-18H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7m0-18v18' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none'><path stroke={stroke} strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 3h7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-7m0-18H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7m0-18v18' /></svg>
   )
 }
 
 export const ArrowLeftIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#353535' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.667' d='M15.833 10H4.167M10 15.833 4.167 10 10 4.167' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#353535' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.667' d='M15.833 10H4.167M10 15.833 4.167 10 10 4.167' /></svg>
   )
 }
 
 export const SunIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><g stroke='#000' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.667' clip-path='url(#a)'><path d='M10 14.167a4.167 4.167 0 1 0 0-8.334 4.167 4.167 0 0 0 0 8.334ZM10 .833V2.5M10 17.5v1.667M3.517 3.517 4.7 4.7M15.3 15.3l1.183 1.183M.833 10H2.5M17.5 10h1.667M3.517 16.483 4.7 15.3M15.3 4.7l1.183-1.183' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h20v20H0z' /></clipPath></defs></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><g stroke='#000' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.667' clipPath='url(#a)'><path d='M10 14.167a4.167 4.167 0 1 0 0-8.334 4.167 4.167 0 0 0 0 8.334ZM10 .833V2.5M10 17.5v1.667M3.517 3.517 4.7 4.7M15.3 15.3l1.183 1.183M.833 10H2.5M17.5 10h1.667M3.517 16.483 4.7 15.3M15.3 4.7l1.183-1.183' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h20v20H0z' /></clipPath></defs></svg>
   )
 }
 
 export const MoonIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#000' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.667' d='M17.5 10.658A7.5 7.5 0 1 1 9.342 2.5a5.833 5.833 0 0 0 8.158 8.158Z' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><path stroke='#000' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.667' d='M17.5 10.658A7.5 7.5 0 1 1 9.342 2.5a5.833 5.833 0 0 0 8.158 8.158Z' /></svg>
   )
 }
 
 export const GiftIcon: React.FC = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><g stroke='#fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.667' clip-path='url(#a)'><path d='M16.667 10v8.333H3.333V10M18.333 5.833H1.667V10h16.666V5.833ZM10 18.333v-12.5M10 5.833H6.25a2.083 2.083 0 1 1 0-4.166c2.917 0 3.75 4.166 3.75 4.166ZM10 5.833h3.75a2.083 2.083 0 0 0 0-4.166c-2.917 0-3.75 4.166-3.75 4.166Z' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h20v20H0z' /></clipPath></defs></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none'><g stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.667' clipPath='url(#a)'><path d='M16.667 10v8.333H3.333V10M18.333 5.833H1.667V10h16.666V5.833ZM10 18.333v-12.5M10 5.833H6.25a2.083 2.083 0 1 1 0-4.166c2.917 0 3.75 4.166 3.75 4.166ZM10 5.833h3.75a2.083 2.083 0 0 0 0-4.166c-2.917 0-3.75 4.166-3.75 4.166Z' /></g><defs><clipPath id='a'><path fill='#fff' d='M0 0h20v20H0z' /></clipPath></defs></svg>
   )
 }
