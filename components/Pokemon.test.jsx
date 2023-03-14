57b7f0a5ab9dcbd64381dd63d9b49be954184449
Victor Villazón <victorzn23@gmail.com>
2023-03-14 09:49:27 -0500

    tests: Pokemon - it should add a pokemon to favorites, failed

---
+++ components/Pokemon.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, screen, cleanup } from '@testing-library/react'
+import { describe, it, beforeEach } from 'vitest'
+import userEvent from '@testing-library/user-event'
+
+import { Pokemon } from './Pokemon'
+
+describe('Pokemon', () => {
+  beforeEach(cleanup)
+
+  it('should add a pokemon to favorites', async () => {
+    const user = userEvent.setup()
+
+    const mockPokemons = {
+      id: 1,
+      name: 'pokemon one',
+      imageUrl: 'pokemon-one-url',
+      types: [
+        { id: 1, name: 'grass' },
+        { id: 2, name: 'water' }
+      ]
+    }
+
+    render(<Pokemon pokemon={mockPokemons} />)
+
+    const button = screen.getByRole('button')
+
+    await user.click(button)
+
+    screen.getByTestId('is-favorite')
+  })
+})
